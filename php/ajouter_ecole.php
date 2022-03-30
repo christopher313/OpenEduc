18bc75f7af63c8f6105cdeb32e167ae2982ea7fa
christopher <christopher.gauger.pro@gmail.com>
2022-03-30 15:07:16 +0100

    Amélioration formulaire d'ajouts d'écoles.

--- php/ajouter_ecole.php
+++ php/ajouter_ecole.php
@@ -28,19 +28,21 @@ if($_SESSION['role'] == 1 || 2){?>
 <body>
     <h1 class="title-h1">Ajouter une école</h1>
 
+   
     <div class="page_container">
         <form action="ajouter_ecole_traitement.php" method="post" class="formulaire_connexion">
-            <input class="champ" type="text" placeholder="Nom de l'école" name="school_name">
-            <input class="champ" type="text" placeholder="Référence" name="school_ref">
-            <input class="champ" type="text" placeholder="Adresse" name="school_adresse">
-            <input class="champ" type="text" placeholder="Code Postal" name="school_cp">
-            <input class="champ" type="text" placeholder="Ville" name="school_city">
-            <input class="champ" type="text" placeholder="Mail" name="school_mail">
-            <input class="champ" type="text" placeholder="Numéro de téléphone" name="school_number">
-            <input type="checkbox" id="maternelle" sname="maternelle" >
-            <label for="school_type">Maternelle</label>
-            <input type="checkbox" id="primaire" sname="primaire">
-            <label for="school_type">Primaire</label>
+            <label style="font-size: 1.5em; color: red">*<input class="champ" type="text" placeholder="Nom de l'école" name="school_name" required></label>
+            <label style="font-size: 1.5em; color: red">*<input class="champ" type="text" placeholder="Référence" name="school_ref"required></label>
+            <label style="font-size: 1.5em; color: red">*<input class="champ" type="text" placeholder="Adresse" name="school_adresse"required></label>
+            <label style="font-size: 1.5em; color: red">*<input class="champ" type="number" placeholder="Code Postal" name="school_cp"required></label>
+            <label style="font-size: 1.5em; color: red">*<input class="champ" type="text" placeholder="Ville" name="school_city"required></label>
+            <label style="font-size: 1.5em; color: red">*<input class="champ" type="mail" placeholder="Mail" name="school_mail"required></label>
+            <label style="font-size: 1.5em; color: red">*<input class="champ" type="tel" placeholder="Numéro de téléphone" name="school_number"required></label>
+            <label style="text-align: left; font-size: 0.8em; margin-bottom: 15px;">Les champs avec un astérique, sont des champs obligatoires.</label>
+            <label for="school_type"><input type="checkbox" id="maternelle" sname="maternelle" >
+            Maternelle</label>
+            <label for="school_type"><input type="checkbox" id="primaire" sname="primaire">
+            Primaire</label>
             <input class="button" type="submit" value="Ajouter" name="bouton_envoie">
             
         </form>
